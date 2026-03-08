7e735bc6b4c5cf07db70483eed6044977dd5e7e2
toasty c# <165564837+ToastyVRthemodmaker@users.noreply.github.com>
2026-03-08 12:14:35 +0100

    Refactor script.js to update logs and functions
    Updated console log message and modified the buy function. Removed example button interaction and API request handling.

--- public/script.js
+++ public/script.js
@@ -1,30 +1,9 @@
-console.log("Website loaded");
+console.log("Northbound loaded");
 
-/* Example button interaction */
-function showMessage() {
-alert("Hello! Thanks for visiting my website.");
+function buy(tool){
+
+alert("Opening " + tool + " page...");
+
+/* later you can redirect to store pages */
+
 }
-
-/* Example API request to backend */
-async function loadInfo() {
-try {
-const response = await fetch("/api/info");
-const data = await response.json();
-
-```
-const infoBox = document.getElementById("apiData");
-if (infoBox) {
-  infoBox.innerText =
-    "Name: " + data.name + " | Site: " + data.site;
-}
-```
-
-} catch (err) {
-console.error("API request failed:", err);
-}
-}
-
-/* Run when page loads */
-window.onload = () => {
-loadInfo();
-};
